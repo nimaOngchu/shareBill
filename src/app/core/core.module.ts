8823deb85495d17ea55840e94e6443e6fbe745a8
nimaOngchu <nima.ongchu@gmail.com>
2018-10-20 17:59:56 +1000

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+@NgModule({
+  imports: [
+
+  ],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent]
+})
+export class CoreModule { }
